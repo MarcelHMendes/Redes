7556c69ac4f24f7f3b0cfe8d84131b93fff3b228
Marcel <marcelmendes@dcc.ufmg.br>
2018-08-15 22:28:27 -0300

    cliente e servidor OK, falta colocar de acordo com a especificacao

--- TP0/testeServerTP0.c
+++ TP0/testeServerTP0.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <pthread.h>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -23,11 +25,11 @@ char *decifra(char *mensagem,int chave){ //Cifra de Cesar
 	return cifra;
 }
 
+void *connection_handler(void *);
 
 int main(int argc, char *argv[]){
 	int sock,i,i2,a;
 	int port = atoi(argv[1]);
-	char *msgDecript = (char *) malloc(TAM_MAX * sizeof(char));
 	 
 
 	sock = socket(AF_INET,SOCK_STREAM,0);
@@ -56,31 +58,50 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
+	
+	struct sockaddr_in cliente_addr;
+	socklen_t cliente_len = sizeof(struct sockaddr_in);
+	pthread_t thread_id;
+
+
+	while(a = accept(sock, (struct sockaddr *) &cliente_addr, &cliente_len)){
+		if(a == -1){
+			perror("accept");
+			exit(1);
+		}
+
+		if(pthread_create(&thread_id,NULL , connection_handler, (void *) &a) < 0){
+			perror("pthread_create");
+			exit(1);
+		}
+
+		
+
+		//close(a);
+	}
+
+
+	exit(0);
+}
+
+void *connection_handler(void *socket_desc){
+
+	int sock = *(int*)socket_desc;
+	char *msgDecript = (char *) malloc(TAM_MAX * sizeof(char));
 	char buf[512];
 	uint32_t tamMsg;
 	uint32_t chave;
 
-	struct sockaddr_in cliente_addr;
-	socklen_t cliente_len = sizeof(struct sockaddr_in);
-
-	a = accept(sock, (struct sockaddr *) &cliente_addr, &cliente_len);
-	if(a == -1){
-		perror("accept");
-		exit(1);
-	}
-
-	size_t c = recv(a,&tamMsg , sizeof(int), MSG_WAITALL);
-	size_t c2 = recv(a,buf,ntohl(tamMsg),MSG_WAITALL);
-	size_t c3 = recv(a,&chave,sizeof(int),MSG_WAITALL);	
+	size_t c = recv(sock,&tamMsg , sizeof(int), MSG_WAITALL);
+	size_t c2 = recv(sock,buf,ntohl(tamMsg),MSG_WAITALL);
+	size_t c3 = recv(sock,&chave,sizeof(int),MSG_WAITALL);	
 
 	msgDecript = decifra(buf, (int)ntohl(chave));
 
-	if(send(a, msgDecript, strlen(msgDecript), 0) < 0){
+	if(send(sock, msgDecript, strlen(msgDecript), 0) < 0){
 		perror("send");
 		exit(1);
 	}
 
-	close(a);
-
-	exit(0);
+	close(sock);
 }
